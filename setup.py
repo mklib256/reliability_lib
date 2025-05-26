8857a8bc58d0a578c7a7e97dc4e4d95f2e0beaa2
mklib256 <kisuule01@gmail.com>
2025-05-26 16:38:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="mklib",
     version="0.1",
-    packages=find_packages(),
+    packages=["reliability_lib"],
     install_requires=[],
     author="Mikka Kisuule",
     description="A library of functions to be used in the evaluation of reliability in distribution networks",
